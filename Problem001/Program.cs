bae137c1bac12776ed101ded30a267e0ec82acb3
Hans Brigman <datyedyeguy@gmail.com>
2012-09-08 08:05:21 -0400

    Problem 01 solved

--- Problem001/Program.cs
+++ Problem001/Program.cs
@@ -9,6 +9,44 @@ namespace Problem001
     {
         static void Main(string[] args)
         {
+            int three = 3;
+            int five = 0;
+            int total = 3;
+            int max = 1000;
+
+            while (true)
+            {
+                if (three >= max && five >= max)
+                    break;
+
+                if (three < five)
+                {
+                    three += 3;
+
+                    if (three == five)
+                        three += 3;
+
+                    if (three >= max)
+                        continue;
+
+                    total += three;
+                }
+                else
+                {
+                    five += 5;
+
+                    if (five == three)
+                        five += 5;
+
+                    if (five >= max)
+                        continue;
+
+                    total += five;
+                }
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
         }
     }
 }
