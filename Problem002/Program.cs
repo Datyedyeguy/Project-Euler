8f388228e7327ef15d070339d45e311886c37505
Hans Brigman <datyedyeguy@gmail.com>
2012-09-08 11:21:49 -0400

    Updated solution to include new project
    Solved problem 2

--- Problem002/Program.cs
+++ Problem002/Program.cs
@@ -9,6 +9,23 @@ namespace Problem002
     {
         static void Main(string[] args)
         {
+            long first = 1;
+            long second = 2;
+            long temp = 0;
+            long total = 0;
+
+            while (second < 4000000)
+            {
+                if ((second & 1) != 1)
+                    total += second;
+
+                temp = first + second;
+                first = second;
+                second = temp;
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
         }
     }
 }
