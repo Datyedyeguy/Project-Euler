14b9f08daded319558feb8d70f6b68b542169e7f
Hans Brigman <datyedyeguy@gmail.com>
2012-09-08 14:09:23 -0400

    Solved problem 4

--- Problem004/Program.cs
+++ Problem004/Program.cs
@@ -9,6 +9,48 @@ namespace Problem004
     {
         static void Main(string[] args)
         {
+            int first = 999;
+            int second = 999;
+            int maxTotal = first * second;
+            int maxTotalBest = 0;
+
+            while (true)
+            {
+                if (IsPalindrome(maxTotal))
+                {
+                    if (maxTotalBest < maxTotal)
+                        maxTotalBest = maxTotal;
+                }
+
+                second--;
+
+                if (second < 100)
+                {
+                    second = first;
+                    first--;
+
+                    if (second * first < maxTotalBest)
+                        break;
+                }
+
+                maxTotal = first * second;
+            }
+
+            Console.WriteLine("FOUND: {0}", maxTotalBest);
+            Console.ReadLine();
+        }
+
+        private static bool IsPalindrome(int value)
+        {
+            string strValue = value.ToString();
+
+            for (int i = 0; i < strValue.Length / 2; i++)
+            {
+                if (strValue[i] != strValue[strValue.Length - i - 1])
+                    return false;
+            }
+
+            return true;
         }
     }
 }
