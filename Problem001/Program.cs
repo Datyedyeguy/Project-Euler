c8a9246affa6b392ecee06ab633b963a58efa375
Hans Brigman <datyedyeguy@gmail.com>
2012-09-08 11:12:39 -0400

    Fixed issue where max could be more than first increment

--- Problem001/Program.cs
+++ Problem001/Program.cs
@@ -10,10 +10,10 @@ namespace Problem001
         static void Main(string[] args)
         {
             // Initial setup, let's start counting with 3 already incremented once.
-            int three = 3;
+            int three = 0;
             int five = 0;
-            int total = 3;
-            int max = 1000;
+            int total = 0;
+            int max = 10;
 
             while (true)
             {
