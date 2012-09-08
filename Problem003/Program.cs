1fec16a9439d7a07ca8f414046bc315bf4d3f286
Hans Brigman <datyedyeguy@gmail.com>
2012-09-08 13:45:06 -0400

    Solved problem 3

--- Problem003/Program.cs
+++ Problem003/Program.cs
@@ -9,6 +9,23 @@ namespace Problem003
     {
         static void Main(string[] args)
         {
+            long largeNumber = 600851475143L;
+            long start = 3;
+            long max = 0;
+
+            while (largeNumber > max)
+            {
+                while (largeNumber % start == 0)
+                {
+                    largeNumber /= start;
+                    max = start;
+                }
+
+                start += 2;
+            }
+
+            Console.WriteLine(max);
+            Console.ReadLine();
         }
     }
 }
